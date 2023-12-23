2e38fb8bbddc0a83e62bb0beeaa878e6bd05f573
Jay-SCM <john.farmer1086@gmail.com>
2023-12-23 16:49:59 +0800

    init commit

--- components/Weather.js
+++ components/Weather.js
@@ -201,8 +201,81 @@
 // export default Weather;
 
 // Weather.js
+// import React, { useState } from 'react';
+// import axios from 'axios';
+// import styles from '../styles/Weather.module.css';
+//
+// const Weather = () => {
+//     const [city, setCity] = useState('');
+//     const [weatherData, setWeatherData] = useState(null);
+//
+//     const getWeatherData = async () => {
+//         try {
+//             const apiKey = 'a16b4275e7f9c699970970348e3b1f9c';
+//             const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+//             const response = await axios.get(apiUrl);
+//             setWeatherData(response.data);
+//         } catch (error) {
+//             console.error('Error fetching weather data:', error);
+//         }
+//     };
+//
+//     const videoSource = 'weathervideo.mp4';
+//
+//     return (
+//         <div className={styles.pageContainer}>
+//             <video autoPlay loop muted className={styles.videoBackground}>
+//                 <source src={videoSource} type="video/mp4" />
+//                 Your browser does not support the video tag.
+//             </video>
+//             <div className={styles.outerCard}>
+//                 <div className={styles.innerCard}>
+//                     <h1 className={styles.heading}>Kliea-Weather-App</h1>
+//                     <div className={styles.inputContainer}>
+//                         <input
+//                             type="text"
+//                             placeholder="Enter city"
+//                             value={city}
+//                             onChange={(e) => setCity(e.target.value)}
+//                             className={styles.searchInput}
+//                         />
+//                         <button onClick={getWeatherData} className={styles.searchButton}>
+//                             Get Weather
+//                         </button>
+//                     </div>
+//                     {weatherData && (
+//                         <div className={styles.weatherCard}>
+//                             <h2 className={styles.weatherTitle}>
+//                                 {weatherData.name}, {weatherData.sys.country}
+//                             </h2>
+//                             <p className={styles.weatherText}>
+//                                 Temperature: {weatherData.main.temp}°C
+//                             </p>
+//                             <p className={styles.weatherText}>
+//                                 Weather: {weatherData.weather[0].description}
+//                             </p>
+//                         </div>
+//                     )}
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+//
+// export default Weather;
+
+
+
+
+// import font Awesome added weather icons.
+
+
+
+// Weather.js
 import React, { useState } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThermometer, faCloud } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/Weather.module.css';
 
 const Weather = () => {
@@ -228,38 +301,37 @@ const Weather = () => {
                 <source src={videoSource} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
-            <div className={styles.outerCard}>
-                <div className={styles.innerCard}>
-                    <h1 className={styles.heading}>Kliea-Weather-App</h1>
-                    <div className={styles.inputContainer}>
-                        <input
-                            type="text"
-                            placeholder="Enter city"
-                            value={city}
-                            onChange={(e) => setCity(e.target.value)}
-                            className={styles.searchInput}
-                        />
-                        <button onClick={getWeatherData} className={styles.searchButton}>
-                            Get Weather
-                        </button>
-                    </div>
-                    {weatherData && (
-                        <div className={styles.weatherCard}>
-                            <h2 className={styles.weatherTitle}>
-                                {weatherData.name}, {weatherData.sys.country}
-                            </h2>
-                            <p className={styles.weatherText}>
-                                Temperature: {weatherData.main.temp}°C
-                            </p>
-                            <p className={styles.weatherText}>
-                                Weather: {weatherData.weather[0].description}
-                            </p>
-                        </div>
-                    )}
+            <div className={styles.innerCard}>
+                <h1 className={styles.heading}>Kliea-Weather-App</h1>
+                <div className={styles.inputContainer}>
+                    <input
+                        type="text"
+                        placeholder="Enter city"
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                        className={styles.searchInput}
+                    />
+                    <button onClick={getWeatherData} className={styles.searchButton}>
+                        Get Weather
+                    </button>
                 </div>
+                {weatherData && (
+                    <div className={styles.weatherCard}>
+                        <h2 className={styles.weatherTitle}>
+                            {weatherData.name}, {weatherData.sys.country}
+                        </h2>
+                        <p className={styles.weatherText}>
+                            <FontAwesomeIcon icon={faThermometer} /> Temperature: {weatherData.main.temp}°C
+                        </p>
+                        <p className={styles.weatherText}>
+                            <FontAwesomeIcon icon={faCloud} /> Weather: {weatherData.weather[0].description}
+                        </p>
+                    </div>
+                )}
             </div>
         </div>
     );
 };
 
 export default Weather;
+
